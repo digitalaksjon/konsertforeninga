7115a435b3412a6c94284e737fb9f24baeb818c8
Digital Aksjon <post@digitalaksjon.no>
2021-05-10 16:42:06 +0200

    Fixed most errors

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       options: {
         ...clientConfig.sanity,
         token: process.env.SANITY_READ_TOKEN,
-        watchMode: !isProd,
+        watchMode: true,
         overlayDrafts: !isProd,
       },
     },
