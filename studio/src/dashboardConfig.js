0785d161f214ea358f732f171e5f0b3c1d88e67c
Digital Aksjon <post@digitalaksjon.no>
2021-05-26 13:41:48 +0200

    fixed some dashboard stuff

--- studio/src/dashboardConfig.js
+++ studio/src/dashboardConfig.js
@@ -12,16 +12,16 @@ export default {
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
                 {
-                  buildHookId: '600ee742ad07d4ea04cec520',
-                  title: 'Konsertforeninga Studio',
-                  name: 'konsertforeninga-studio',
-                  apiId: '14e257b5-5079-45f4-bfb6-32474555b2b9'
-                },
-                {
                   buildHookId: '600ee742d04e2ff18e12771b',
                   title: 'Konsertforeninga Nettside',
                   name: 'konsertforeninga',
                   apiId: '461be2c7-8e92-4d12-8120-85350265d64f'
+                },
+                {
+                  buildHookId: '600ee742ad07d4ea04cec520',
+                  title: 'Konsertforeninga Studio',
+                  name: 'konsertforeninga-studio',
+                  apiId: '14e257b5-5079-45f4-bfb6-32474555b2b9'
                 }
               ]
             }
@@ -40,7 +40,7 @@ export default {
     { name: 'project-users', layout: { height: 'auto' } },
     {
       name: 'document-list',
-      options: { title: 'Siste konserter', order: '_publishedAt asc', types: ['concert'] },
+      options: { title: 'Siste konserter', order: 'concertDateTime desc', types: ['concert'] },
       layout: { width: 'medium' }
     }
   ]
