da984db4e74f0b663382213399def7c5490cd23c
digitalaksjon <post@digitalaksjon.no>
2022-01-14 17:59:47 +0100

    Update index.tsx

--- web/src/containers/sidebar/index.tsx
+++ web/src/containers/sidebar/index.tsx
@@ -81,6 +81,7 @@ const Sidebar: React.FunctionComponent<SidebarProps> = () => {
   const Posts = Data.posts.edges;
 
   return (
+    /*
     <SidebarWrapper>
       <SidebarWidget>
         <div className="promoImage">
@@ -88,7 +89,7 @@ const Sidebar: React.FunctionComponent<SidebarProps> = () => {
 
         </div>
       </SidebarWidget>
-
+*/
       <SidebarWidget>
         <WidgetTitle>Nytt fra foreninga</WidgetTitle>
         {Posts.map(({ node }: any) => {
