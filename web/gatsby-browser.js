60ba25cefe5d76788051d823e4be120744cb02e4
digitalaksjon <post@digitalaksjon.no>
2022-01-26 14:35:41 +0100

    added force browser reload

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -3,3 +3,5 @@ import 'typeface-montserrat'
 import 'typeface-merriweather'
 require('prismjs/themes/prism.css')
 require('katex/dist/katex.min.css')
+
+export function onServiceWorkerUpdateReady(){window.location.reload(true)};
