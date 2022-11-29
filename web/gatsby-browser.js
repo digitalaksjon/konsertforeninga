1deaf1965d923301f984f9652dd34264aa15e285
digitalaksjon <post@digitalaksjon.no>
2022-11-29 20:16:03 +0100

    fix cache

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -4,5 +4,11 @@ import 'typeface-merriweather'
 require('prismjs/themes/prism.css')
 require('katex/dist/katex.min.css')
 
-// trigger an immediate page refresh when an update is found
-export const onServiceWorkerUpdateReady = () => window.location.reload();
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    "This website has been updated. Reload to display the latest version?"
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
